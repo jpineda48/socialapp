8db07f801cd0a0f9bcf845425809ad246fa5d46d
jpineda48 <jacquelinepineda48@gmail.com>
2023-09-22 22:19:59 -0400

    signup and login

--- main_app/models.py
+++ main_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-# Create your models here.
 
+# Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user= models.IntegerField
+    id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg= models.ImageField(upload_to='profile_images', default='book-icon.png')
-    location= models.CharField(max_length=100, blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='media/book-icon.png')
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
